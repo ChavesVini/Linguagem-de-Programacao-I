2be38eb5755c88a7e6d7f944c256bd9e5613c9b2
ChavesVini <vinidasilva2004@outlook.com>
2024-01-04 11:43:53 -0300

    :coffin: Removed EXCEPTIONS file, never used
    :coffin: removed EXCEPTIONS file, never used

--- EXCEPTIONS/ReversedPhrase.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-import java.lang.StringBuilder;
-
-public class ReversedPhrase {
-    public String Reversed(String phrase) {
-        return StringBuilder(phrase).reverse();
-    }        
-}
